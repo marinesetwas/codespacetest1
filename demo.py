d7d65e8ef61cb1fd4ba60dbc22baba95a559f5dc
Hans-Jörg Stangor <marinesetwas@yahoo.de>
2023-03-16 12:29:01 +0000

    Added demo.py as a short python demo

---
+++ demo.py
@@ -0,0 +1,9 @@
+# This is a Python File i would like to try inside Codespaces
+
+print("-----------")
+print("Hello World")
+print("-----------")
+print("Let's count to 10")
+for i in range(1,11):
+    print(i,"...",sep="",end="")
+print("IGNITION!")
